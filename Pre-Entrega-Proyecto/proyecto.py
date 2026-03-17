01fad0a223505b214f9bd1a2a8ddd53a34be4e3e
PabloCarrai <pablohernanjuan@hotmail.com>
2026-03-17 20:06:47 -0300

    Esto estaria ok

--- Pre-Entrega-Proyecto/proyecto.py
+++ Pre-Entrega-Proyecto/proyecto.py
@@ -31,12 +31,49 @@ while True:
             print("Elegistes 'Mostrar productos'")
             for elementos in range(len(nombres)):
                 print(
-                    f"Producto: {nombres[elementos]} Categoria: {categorias[elementos]}, Precio:{precios[elementos]}"
+                    f"Producto: {nombres[elementos]} Categoria: {categorias[elementos]}, Precio: ${precios[elementos]}"
                 )
         case "3":
             print("Elegistes 'Buscar productos'")
+            for elementos in range(len(nombres)):
+                print(
+                    f"Producto: {nombres[elementos]} Categoria: {categorias[elementos]}, Precio: ${precios[elementos]}"
+                )
+            nombre = input("Nombre del producto? ").strip().capitalize()
+            if nombre:
+                if nombre in nombres:
+                    indice = nombres.index(nombre)
+                    print(f"Informacion del producto {nombre}")
+                    print(f"Categoria {categorias[indice]}")
+                    print(f"Precio {precios[indice]}")
+                else:
+                    print("Elegiste un producto que no existe.")
+                    continue
+            else:
+                print("Elegiste un producto que no existe.")
+                break
         case "4":
             print("Elegistes 'Eliminar producto'")
+            print("Estos son los productos existentes actualmente.")
+            for elementos in range(len(nombres)):
+                print(
+                    f"Producto: {nombres[elementos]} Categoria: {categorias[elementos]}, Precio: ${precios[elementos]}"
+                )
+            nombre = input("Nombre del producto a eliminar? ").strip().capitalize()
+            if nombre:
+                if nombre in nombres:
+                    indice = nombres.index(nombre)
+                    del nombres[indice]
+                    del categorias[indice]
+                    del precios[indice]
+                    print("Producto eliminado  ")
+                else:
+                    print("Elegiste un producto que no existe.")
+                    break
+            else:
+                print("Elegiste un producto que no existe.")
+                break
+
         case "5":
             print("Elegistes 'Salir'")
             print("Hasta Luego")
