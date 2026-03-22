c8fcfe382122ad1ebb3febcdb331bafaf1174d22
PabloCarrai <pablohernanjuan@hotmail.com>
2026-03-22 20:49:49 -0300

    va

--- Clase-13/Ejercicio/ejercicio1.py
+++ Clase-13/Ejercicio/ejercicio1.py
@@ -90,6 +90,23 @@ def mostrar_registros(db):
     conexion.close()
 
 
+def modificar_registro(db, producto, precio, id):
+    conexion = sqlite3.connect(db)
+    #   Creamos un cursor de la conexion
+    cursor = conexion.cursor()
+    datos = (producto, precio, id)
+    sentencia = "update productos set nombre=?, precio=? where id=?"
+    #   Ejecutamos la sentencia que crea una tabla productos si esta no existe
+    cursor.execute(sentencia, datos)
+    #   Hacemos efectivos los cambios
+    conexion.commit()
+    #   Cerramos el cursor
+    cursor.close()
+    #   Cerramos la conexion
+    conexion.close()
+    print(f"Dato modificado, {datos}")
+
+
 def validar_datos(nombre, precio):
     if (
         isinstance(nombre, str)
@@ -117,6 +134,22 @@ def validar_id_exista_db(db, id):
     cursor.close()
     #   Cerramos la conexion
     conexion.close()
+
+
+def eliminar_registro(db, id):
+    conexion = sqlite3.connect(db)
+    #   Creamos un cursor de la conexion
+    cursor = conexion.cursor()
+    sentencia = "delete from productos where id=?"
+    #   Ejecutamos la sentencia que crea una tabla productos si esta no existe
+    cursor.execute(sentencia, (id,))
+    #   Hacemos efectivos los cambios
+    conexion.commit()
+    #   Cerramos el cursor
+    cursor.close()
+    #   Cerramos la conexion
+    conexion.close()
+    print("Dato eliminado")
 
 
 def mostrar_menu():
@@ -152,13 +185,42 @@ def mostrar_menu():
                 id = input("Ingrese el id del producto a modificar   ")
                 if id:
                     print("Es un id no vacio")
-                    print(id)
-                    resultado = validar_id_exista_db(ruta_db, id)
-                    print(f"{len(resultado)}, {resultado}")
+                    try:
+                        resultado = validar_id_exista_db(ruta_db, id)
+                        if resultado == None:
+                            print("No hay datos")
+                        else:
+                            nombre = input("Nombre?   ")
+                            precio = input("Precio? ")
+                            try:
+                                precio = int(precio)
+                            except ValueError:
+                                print("Problema con el precio")
+                            if validar_datos(nombre, precio):
+                                modificar_registro(ruta_db, nombre, precio, id)
+                            else:
+                                print("Problema con los datos. ")
+                    except Exception as e:
+                        print(f"Error {e}")
                 else:
                     print("Id vacio")
             case "4":
-                pass
+                mostrar_registros(ruta_db)
+                id = input("Ingrese el id del producto a eliminar   ")
+                if id:
+                    print("Es un id no vacio")
+                    try:
+                        resultado = validar_id_exista_db(ruta_db, id)
+                        if resultado == None:
+                            print("No hay datos")
+                        else:
+                            eliminar_registro(ruta_db, id)
+                            print("Eliminar registro")
+                            pass
+                    except Exception as e:
+                        print(f"Error {e}")
+                else:
+                    print("Id vacio")
             case "5":
                 print("Saliendo del programa...")
                 break
