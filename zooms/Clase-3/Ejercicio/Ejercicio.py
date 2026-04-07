a2d1c8bf4e165a5ced9d37e9c6a8d6da58dd79b8
PabloCarrai <pablohernanjuan@hotmail.com>
2026-04-06 22:06:55 -0300

    va

---
+++ zooms/Clase-3/Ejercicio/Ejercicio.py
@@ -0,0 +1,10 @@
+edad = int(input("Ingrese su edad  "))
+if edad > 20:
+    print("Puede pasar  ")
+
+respuesta = input("Tiene tarjeta ID? s/n  ")
+if respuesta == "s":
+    print("Puede pasar ")
+else:
+    print("No puede pasar ")
+
